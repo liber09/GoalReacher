458f32a7a00991e400ba5d40c93b2c4ce9d11dca
liber09 <linda.bergsangel@hotmail.com>
2023-05-07 11:04:25 +0200

    validation for register

--- GoalReacher/ViewModels/RegisterViewModel.swift
+++ GoalReacher/ViewModels/RegisterViewModel.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+class RegisterViewModel: ObservableObject{
+    @Published var email = ""
+    @Published var name = ""
+    @Published var password = ""
+    
+    init() {}
+    
+    func register() {
+        guard validate() else {
+            return
+        }
+    }
+    
+    private func validate() -> Bool {
+        guard !name.trimmingCharacters(in: .whitespaces).isEmpty,
+              !email.trimmingCharacters(in: .whitespaces).isEmpty,
+              !password.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return false
+        }
+        
+        guard email.contains("@") && email.contains(".") else {
+            return false
+        }
+        
+        guard password.count >= 6 else {
+                return false
+        }
+        
+        return true
+    }
+}
